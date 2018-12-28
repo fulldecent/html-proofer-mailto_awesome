696f3fe3cf52990f6c16f6d876e1330b72cdf3ba
William Entriken <github.com@phor.net>
2018-12-28 11:31:29 -0500

    Use canonical sentence structure for spec tests

--- spec/mailto_awesome_spec.rb
+++ spec/mailto_awesome_spec.rb
@@ -2,13 +2,13 @@ require_relative 'spec_helper'
 
 RSpec.describe ::MailtoAwesome do
 
-  it 'Good mailto link' do
+  it 'accepts an awesome mailto link' do
     file = "#{FIXTURES_DIR}/mailto_awesome.html"
     proofer = run_proofer(file, :file, {:check_mailto_awesome => true})
     expect(proofer.failed_tests.last).to eq nil
   end
 
-  it 'Bad mailto link' do
+  it 'rejects a not-awesome mailto link' do
     file = "#{FIXTURES_DIR}/mailto_not_awesome.html"
     proofer = run_proofer(file, :file, {:check_mailto_awesome => true})
     expect(proofer.failed_tests.last).to match(%r{This is a not-awesome mailto link!})
